d22eb6c257745342a59f5b983ff23f6e006bf618
Priyanshu Barnwal <barnwal.priyanshu4@gmail.com>
2025-10-08 10:14:29 +0530

    setup clerk

--- components/TiptapEditor.tsx
+++ components/TiptapEditor.tsx
@@ -11,6 +11,15 @@ import { FaUnderline } from 'react-icons/fa';
 import { BiUndo } from 'react-icons/bi';
 import { BiRedo } from 'react-icons/bi';
 
+import {
+  ClerkProvider,
+  SignInButton,
+  SignUpButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
+
 // MenuBar component for toolbar buttons
 type MenuBarProps = {
   // The editor can be null initially, so we use 'Editor | null'
@@ -23,40 +32,56 @@ const MenuBar = ({ editor }: MenuBarProps) => {
   }
 
   return (
-    <div className="menu-bar">
-      <button
-        onClick={() => editor.chain().focus().toggleBold().run()}
-        disabled={!editor.can().chain().focus().toggleBold().run()}
-        className={editor.isActive('bold') ? 'is-active' : ''}
-      >
-        <FaBold/>
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleItalic().run()}
-        disabled={!editor.can().chain().focus().toggleItalic().run()}
-        className={editor.isActive('italic') ? 'is-active' : ''}
-      >
-        <FaItalic/>
-      </button>
-      <button
-        onClick={() => editor.chain().focus().toggleUnderline().run()}
-        className={editor.isActive('underline') ? 'is-active' : ''}
-      >
-        <FaUnderline/>
-      </button>
-      <button
-        onClick={() => editor.chain().focus().undo().run()}
-        disabled={!editor.can().chain().focus().undo().run()}
-      >
-        <BiUndo/>
-      </button>
-      <button
-        onClick={() => editor.chain().focus().redo().run()}
-        disabled={!editor.can().chain().focus().redo().run()}
-      >
-        <BiRedo/>
-      </button>
-    </div>
+    <ClerkProvider>
+      <div className="menu-bar">
+        <button
+          onClick={() => editor.chain().focus().toggleBold().run()}
+          disabled={!editor.can().chain().focus().toggleBold().run()}
+          className={editor.isActive('bold') ? 'is-active' : ''}
+        >
+          <FaBold/>
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleItalic().run()}
+          disabled={!editor.can().chain().focus().toggleItalic().run()}
+          className={editor.isActive('italic') ? 'is-active' : ''}
+        >
+          <FaItalic/>
+        </button>
+        <button
+          onClick={() => editor.chain().focus().toggleUnderline().run()}
+          className={editor.isActive('underline') ? 'is-active' : ''}
+        >
+          <FaUnderline/>
+        </button>
+        <button
+          onClick={() => editor.chain().focus().undo().run()}
+          disabled={!editor.can().chain().focus().undo().run()}
+        >
+          <BiUndo/>
+        </button>
+        <button
+          onClick={() => editor.chain().focus().redo().run()}
+          disabled={!editor.can().chain().focus().redo().run()}
+        >
+          <BiRedo/>
+        </button>
+      </div>
+
+      <SignedOut>
+        <SignInButton />
+        {/* <SignUpButton> */}
+          <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+            Sign Up
+          </button>
+        {/* </SignUpButton> */}
+      </SignedOut>
+      
+      {/* <SignedIn>
+              <UserButton />
+      </SignedIn> */}
+
+    </ClerkProvider>
   );
 };
 
