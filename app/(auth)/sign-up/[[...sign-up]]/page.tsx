7a325ac533f750f08ef4c64de4a9bf7ecf1017fb
priyanshubarnw <barnwal.priyanshu4@gmail.com>
2025-12-09 01:41:42 +0530

    took quite a hiatus. will be quicker and more consistent now! This commit completes authentication btw, home page next

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,9 @@
 import { SignUp } from "@clerk/nextjs";
 
-export default function Page() {
+export default function SignUpPage() {
   return (
     <main className="flex h-screen w-full flex-col items-center justify-center gap-10">
       <SignUp />
     </main>
-  )
+  );
 }
